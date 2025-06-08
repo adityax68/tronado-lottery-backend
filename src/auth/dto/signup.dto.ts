511a5b3f41825b53e76e2da17d5feaeabb7f7826
Aditya Maurya <adityamaurya@Adityas-MacBook-Air.local>
2025-06-08 14:30:54 +0530

    added password validation

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,11 +1,15 @@
-// src/auth/dto/signup.dto.ts
-import { IsEmail, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsString, MinLength, Matches } from 'class-validator';
 
 export class SignupDto {
   @IsEmail()
+  @MinLength(8)
   email: string;
 
   @IsString()
-  @MinLength(6)
+  @MinLength(8)
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/, {
+    message:
+      'Password must include at least 1 uppercase letter, 1 lowercase letter, 1 digit, 1 special character, and be at least 8 characters long',
+  })
   password: string;
 }
